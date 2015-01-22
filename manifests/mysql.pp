d1603c369b9242dee39a50667787f68bdc79ebf2
Tom De Vylder <tom@penumbra.be>
2015-01-22 17:09:29 +0100

    MySQL: add grants, users

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,4 +2,6 @@
 #
 class cultuurnet::mysql {
   create_resources(mysql::server::databases, hiera_hash('mysql::server::databases', {}))
+  create_resources(mysql::server::grants, hiera_hash('mysql::server::grants', {}))
+  create_resources(mysql::server::users, hiera_hash('mysql::server::users', {}))
 }
