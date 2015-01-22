0c71c83dc705e6e24bc9875022302f24649a3ff1
Tom De Vylder <tom@penumbra.be>
2015-01-22 11:56:08 +0100

    Add class: puppetmaster

---
+++ manifests/puppetmaster.pp
@@ -0,0 +1,20 @@
+# == Class: cultuurnet::puppetmaster
+#
+class cultuurnet::puppetmaster () {
+  require ::apache
+
+  class {
+    '::apache::mod::headers':;
+    '::apache::mod::passenger':;
+    '::apache::mod::ssl':;
+  }
+
+  file { '/etc/apache2/sites-enabled/puppetmaster.conf':
+    ensure  => present,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('cultuurnet/apache2-puppetmaster.conf.erb'),
+  }
+}
+
