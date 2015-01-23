26210427b801f9c476d88359c7c9930cf00c2ebe
Tom De Vylder <tom@penumbra.be>
2015-01-23 14:03:35 +0100

    Add class icinga2::client

---
+++ manifests/icinga2/client.pp
@@ -0,0 +1,41 @@
+# == Class: cultuurnet::icinga2::client
+#
+class cultuurnet::icinga2::client () {
+  require ::icinga2
+  require ::icinga2::nrpe
+
+  if ! defined(Class['::cultuurnet::icinga2::server']) {
+    notice "Exporting Icinga data for ${::fqdn}"
+    @@::icinga2::object::host { $::fqdn:
+      display_name     => $::fqdn,
+      ipv4_address     => hiera('icinga2::object::host::ipv4_address', $::ipaddress),
+      target_dir       => '/etc/icinga2/objects/hosts',
+      target_file_name => "${::fqdn}.conf",
+      vars             => {
+        distro              => $::operatingsystem,
+        esx_version         => $::esx_version,
+        os                  => $::kernel,
+        virtual_machine     => $::is_virtual,
+        vmware              => $::vmware,
+        vmware_patchlevel   => $::vmware_patchlevel,
+        vmwaretools_version => $::vmwaretools_version,
+      },
+    }
+  }
+
+  create_resources(
+    ::icinga2::object::checkcommand,
+    hiera_hash('icinga2::object::checkcommand', {})
+  )
+
+  create_resources(
+    ::icinga2::checkplugin,
+    hiera_hash('icinga2::checkplugin', {})
+  )
+
+  create_resources(
+    ::icinga2::nrpe::command,
+    hiera_hash('icinga2::nrpe::command', {})
+  )
+}
+
