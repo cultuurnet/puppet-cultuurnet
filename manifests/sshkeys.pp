29ccb754b25c203b942a7db58068aee5c135ec3c
Kristof Willaert <kristof.willaert@cultuurnet.be>
2015-07-16 10:49:37 +0200

    Second argument to hiera_hash must be a hash

--- manifests/sshkeys.pp
+++ manifests/sshkeys.pp
@@ -1,5 +1,5 @@
 # == Class: cultuurnet::sshkeys
 #
 class cultuurnet::sshkeys {
-  create_resources(ssh_authorized_key, hiera_hash('ssh_keys', []))
+  create_resources(ssh_authorized_key, hiera_hash('ssh_keys', {}))
 }
