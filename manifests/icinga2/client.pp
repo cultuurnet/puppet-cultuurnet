313aebd65301f47fdbbf064d517e53f13f5e3b85
Kristof Willaert <kristof.willaert@cultuurnet.be>
2015-08-06 10:48:14 +0200

    Remove check for icinga server

--- manifests/icinga2/client.pp
+++ manifests/icinga2/client.pp
@@ -3,19 +3,16 @@
 class cultuurnet::icinga2::client () {
   require ::icinga2::nrpe
 
-  if ! defined(Class['::cultuurnet::icinga2::server']) {
-    notice "Exporting Icinga data for ${::fqdn}"
-    @@::icinga2::object::host { $::fqdn:
-      display_name     => $::fqdn,
-      ipv4_address     => hiera('icinga2::object::host::ipv4_address', $::ipaddress),
-      target_dir       => '/etc/icinga2/objects/hosts',
-      target_file_name => "${::fqdn}.conf",
-      vars             => {
-        distro              => $::operatingsystem,
-        os                  => $::kernel,
-        virtual_machine     => $::is_virtual,
-        puppet_certname     => $::clientcert,
-      },
+  @@::icinga2::object::host { $::fqdn:
+    display_name     => $::fqdn,
+    ipv4_address     => $::ipaddress,
+    target_dir       => '/etc/icinga2/objects/hosts',
+    target_file_name => "${::fqdn}.conf",
+    vars             => {
+      distro              => $::operatingsystem,
+      os                  => $::kernel,
+      virtual_machine     => $::is_virtual,
+      puppet_certname     => $::clientcert,
     }
   }
 
@@ -29,4 +26,3 @@ class cultuurnet::icinga2::client () {
     hiera_hash('icinga2::nrpe::command', {})
   )
 }
-
