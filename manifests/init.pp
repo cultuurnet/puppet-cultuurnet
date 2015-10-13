e336409b4f0212cd7a118859f47187e48dd5cdb5
Kristof Willaert <kristof.willaert@cultuurnet.be>
2015-10-13 10:22:45 +0200

    Add resource purging for ssh keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,16 @@
 # == Class: cultuurnet
 #
 class cultuurnet {
+
+  resources { 'sshkey':
+    purge => true,
+    noop  => true
+  }
+
   class {
     '::cultuurnet::firewall':;
     '::cultuurnet::sshkeys':;
+    '::cultuurnet::knownhosts':;
     '::cultuurnet::users':;
   }
 }
