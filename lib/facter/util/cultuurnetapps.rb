290a0ce12f98bce7c3db7691cdb906858fcdfa2f
Kristof Willaert <kristof.willaert@cultuurnet.be>
2015-11-20 16:53:25 +0100

    Need to escape newline in command

--- lib/facter/util/cultuurnetapps.rb
+++ lib/facter/util/cultuurnetapps.rb
@@ -4,7 +4,7 @@ module Facter
   module Util
     module CultuurNetApps
       def self.get_version(prefix)
-	command = "dpkg-query -f='\$\{binary:Package\}:\$\{Version\}\n' -W '#{prefix}*'"
+	command = "dpkg-query -f='\$\{binary:Package\}:\$\{Version\}\\n' -W '#{prefix}*'"
 	versions = Facter::Core::Execution.exec(command)
 
 	versions.split("\n").map do |string|
