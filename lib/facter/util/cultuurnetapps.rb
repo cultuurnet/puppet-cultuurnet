98c16dd16ae9df29aaed4dbbc17f3bdc47d982f3
Kristof Willaert <kristof.willaert@cultuurnet.be>
2015-12-09 10:22:22 +0100

    Use spaces, not tabs

--- lib/facter/util/cultuurnetapps.rb
+++ lib/facter/util/cultuurnetapps.rb
@@ -4,16 +4,16 @@ module Facter
   module Util
     module CultuurNetApps
       def self.get_version(prefix)
-	command = "dpkg-query -f='\$\{binary:Package\}:\$\{Version\}\\n' -W '#{prefix}*' 2> /dev/null"
-	versions = Facter::Core::Execution.exec(command)
+        command = "dpkg-query -f='\$\{binary:Package\}:\$\{Version\}\\n' -W '#{prefix}*' 2> /dev/null"
+        versions = Facter::Core::Execution.exec(command)
 
         if versions.empty?
           nil
         else
-	  versions.split("\n").map do |string|
-	    component = string[/(.*):/,1]
-	    version = string[/:(.*)$/,1]
-	    commit = version[/\+sha.(.*)$/,1] || ""
+          versions.split("\n").map do |string|
+            component = string[/(.*):/,1]
+            version = string[/:(.*)$/,1]
+            commit = version[/\+sha.(.*)$/,1] || ""
 
             { component => { 'version' => version, 'commit' => commit} }
           end
