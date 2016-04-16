6bd94262076551462af5d89e9713809a52655205
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-04-17 00:59:18 +0200

    Add update_facts script

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,4 +11,10 @@ class cultuurnet {
   if $settings::storeconfigs {
     class { '::cultuurnet::knownhosts': }
   }
+
+  file { '/usr/local/bin/update_facts':
+    ensure => 'file',
+    mode   => '0755',
+    source => 'file:///modules/cultuurnet/update_facts'
+  }
 }
