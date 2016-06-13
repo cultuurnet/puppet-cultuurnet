2005048124f7ea0c2d62e16eb68be969e1b15f4d
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-06-13 14:10:18 +0200

    Complete move to new PHP module

--- manifests/php.pp
+++ manifests/php.pp
@@ -2,22 +2,8 @@
 #
 class cultuurnet::php {
 
-  case $::environment {
-  'testing':
-    {
-      include php::globals
-      include php
+  include php::globals
+  include php
 
-      Class['php::globals'] -> Class['php']
-    }
-  'production':
-    {
-      include php
-      include php::extension::curl
-      include php::extension::gd
-      include php::extension::intl
-      include php::extension::mysql
-      include php::extension::tidy
-    }
-  }
+  Class['php::globals'] -> Class['php']
 }
