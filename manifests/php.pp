e9e3b0c705b4af689af151377092ce863a0d72c7
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-06-13 11:47:07 +0200

    Fix curly braces

--- manifests/php.pp
+++ manifests/php.pp
@@ -3,32 +3,32 @@
 class cultuurnet::php {
 
   case $::environment {
-    'testing':
-      {
-        class { 'php::globals': }
-        class { 'php':
-          manage_repos => false,
-          fpm          => false,
-          dev          => false,
-          composer     => false,
-          pear         => false,
-          extensions   => {
-            curl  => { },
-            gd    => { },
-            intl  => { },
-            mysql => { },
-            tidy  => { }
-          }
+  'testing':
+    {
+      class { 'php::globals': }
+      class { 'php':
+        manage_repos => false,
+        fpm          => false,
+        dev          => false,
+        composer     => false,
+        pear         => false,
+        extensions   => {
+          curl  => { },
+          gd    => { },
+          intl  => { },
+          mysql => { },
+          tidy  => { }
         }
       }
-    'production':
-      {
-        class { 'php':
-        class { 'php::extension::curl': }
-        class { 'php::extension::gd': }
-        class { 'php::extension::intl': }
-        class { 'php::extension::mysql': }
-        class { 'php::extension::tidy': }
-
-      }
+    }
+  'production':
+    {
+      class { 'php': }
+      class { 'php::extension::curl': }
+      class { 'php::extension::gd': }
+      class { 'php::extension::intl': }
+      class { 'php::extension::mysql': }
+      class { 'php::extension::tidy': }
+    }
+  }
 }
