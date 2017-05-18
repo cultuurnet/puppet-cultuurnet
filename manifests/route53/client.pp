a399dbcfc2671f1ec2cc4daa64eb2b9eb404cf4e
wouter simons <wouter.simons@cultuurnet.be>
2017-05-18 15:13:48 +0200

    fixed - s _ confusion in route53 client.. updater puppet-file

--- manifests/route53/client.pp
+++ manifests/route53/client.pp
@@ -2,7 +2,7 @@ class cultuurnet::route53::client () {
   @@route53_a_record { "${::hostname}.machines.publiq.be.":
       zone	=> 'publiq.be.',
       ttl	=> '300', 
-      values	=> [$::ec2_metadata['public_ipv4']]
+      values	=> [$::ec2_metadata['public-ipv4']]
   }
 }
 
