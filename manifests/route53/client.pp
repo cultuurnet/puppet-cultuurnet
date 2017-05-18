25ef30c2ba97a1ccb0ab3dc0a07d65d3e3888858
wouter simons <wouter.simons@cultuurnet.be>
2017-05-18 15:07:35 +0200

    fixed missing dot in route53 client

--- manifests/route53/client.pp
+++ manifests/route53/client.pp
@@ -1,5 +1,5 @@
 class cultuurnet::route53::client () {
-  @@route53_a_record { "${::hostname}.machines.publiq.be":
+  @@route53_a_record { "${::hostname}.machines.publiq.be.":
       zone	=> 'publiq.be.',
       ttl	=> '300', 
       values	=> [$::ec2_metadata['public_ipv4']]
