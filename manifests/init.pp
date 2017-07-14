dc1ec2c0e2bc3e9c3d75edb0cc40698775faa3de
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-07-14 17:13:55 +0200

    Move update_facts script from cultuurnet to deployment module

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,10 +11,4 @@ class cultuurnet {
   if $settings::storeconfigs {
     class { '::cultuurnet::knownhosts': }
   }
-
-  file { '/usr/local/bin/update_facts':
-    ensure => 'file',
-    mode   => '0755',
-    source => 'puppet:///modules/cultuurnet/update_facts'
-  }
 }
