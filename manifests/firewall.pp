24dedb5eb4a02b336f3383da5ddd4117ecbe632f
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-10-25 22:15:26 +0200

    Move nrpe firewall rule to firewall class

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -8,6 +8,12 @@ class cultuurnet::firewall {
 
     resources { 'firewall': purge => true }
 
+    firewall { '200 accept nrpe traffic':
+      proto  => 'tcp',
+      dport  => '5666',
+      action => 'accept'
+    }
+
     create_resources(firewall, hiera_hash('firewall', {}))
   # }
 }
