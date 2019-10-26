48fb12e13569858f27f381a0d24d4d2c29db4270
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-10-26 14:28:48 +0200

    Remove duplicate declaration

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,12 +2,6 @@
 #
 class cultuurnet::firewall {
 
-  firewall { '200 accept nrpe traffic':
-    proto  => 'tcp',
-    dport  => '5666',
-    action => 'accept'
-  }
-
   firewall { '100 accept ssh traffic':
     proto  => 'tcp',
     dport  => '22',
