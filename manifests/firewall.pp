7746c70c68d0f0ee9e797f167de7cba17029ae42
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-10-26 20:25:40 +0200

    Remove ssh firewall rule

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,11 +2,5 @@
 #
 class cultuurnet::firewall {
 
-  firewall { '100 accept ssh traffic':
-    proto  => 'tcp',
-    dport  => '22',
-    action => 'accept'
-  }
-
   create_resources(firewall, hiera_hash('firewall', {}))
 }
