cb62f61e8633ad0998717ff7866793afee834849
wouter simons <wouter@ScrewLoose.simons.duckdns.org>
2020-07-14 12:28:36 +0200

    added httpsauth pp

---
+++ manifests/resources/httpauth.pp
@@ -0,0 +1,26 @@
+class cultuurnet::resources::httpauth { 
+
+
+   file { '/etc/apache2/uitmetvlieg-htpasswd':
+      ensure => 'file',
+      mod => '0644',
+      } 
+   httpauth { 'admin':
+      file     => '/etc/apache2/uitmetvlieg-htpasswd',
+      password => 'aiphei7Kie8e',
+      require  => 'Class[Apache]',
+      before   => 'File[/etc/apache2/uitmetvlieg-htpasswd]',
+      }
+    httpauth { 'uitmetvlieg':
+      file     => '/etc/apache2/uitmetvlieg-htpasswd',
+      password => 'Umewoo2e',
+      require  => 'Class[Apache]',
+      before   => 'File[/etc/apache2/uitmetvlieg-htpasswd]',
+      }
+    httpauth {'vlieg':
+      file     => '/etc/apache2/uitmetvlieg-htpasswd',
+      password => 'Fei5ooJu',
+      require  => 'Class[Apache]',
+      before   => 'File[/etc/apache2/uitmetvlieg-htpasswd]',
+      }
+}
