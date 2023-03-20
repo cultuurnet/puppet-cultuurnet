92f6658627d13ff81a947444115c8a64b6ed2a26
Kristof Willaert <kristof@publiq.be>
2023-03-20 18:25:07 +0100

    Make zone for machines.publiq.be records configurable

--- manifests/route53/client.pp
+++ manifests/route53/client.pp
@@ -1,10 +1,13 @@
-class cultuurnet::route53::client () {
+class cultuurnet::route53::client (
+  String $zone_name = 'publiq.be'
+) {
+  $zone = regsubst($zone_name, '\.$', '')
 
   if versioncmp( $facts['os']['release']['major'], '20.04') < 0 {
     @@route53_a_record { "${::hostname}.machines.publiq.be.":
-      zone	=> 'publiq.be.',
-      ttl	=> '300',
-      values	=> [ "${::ec2_metadata['public-ipv4']}" ]
+      zone	 => "${zone}.",
+      ttl	   => '300',
+      values => [ "${::ec2_metadata['public-ipv4']}" ]
     }
   }
 }
